bbe834f1c8ab1609a5d59fc38943ad210ad5cd8a
Nancy Jo <enciie@gmail.com>
2018-05-21 00:56:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  # code goes here
+  @@all = []
+
+  attr_accessor :name, :pets
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @pets = {}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.counter
+    @@all.size
+  end
+
 end
